3fcd3d2dda71250bc8c11fe54c3afd131f8e3e5b
Avvrik <avvrik@users.noreply.github.com>
2025-04-20 18:40:23 +0500

    Fix image paths for GitHub Pages deployment

--- src/data/cardData.ts
+++ src/data/cardData.ts
@@ -2,47 +2,47 @@ export const cardBackgrounds = [
   {
     id: 1,
     name: 'Medieval Scene 1',
-    imageUrl: '/images/1664125456_shutok.ru.17210242.jpg'
+    imageUrl: '/medieval-easter-cards-new/images/1664125456_shutok.ru.17210242.jpg'
   },
   {
     id: 2,
     name: 'Medieval Scene 2',
-    imageUrl: '/images/1664125487_shutok.ru.17210232.jpg'
+    imageUrl: '/medieval-easter-cards-new/images/1664125487_shutok.ru.17210232.jpg'
   },
   {
     id: 3,
     name: 'Medieval Scene 3',
-    imageUrl: '/images/1664125518_shutok.ru.17210211.jpg'
+    imageUrl: '/medieval-easter-cards-new/images/1664125518_shutok.ru.17210211.jpg'
   },
   {
     id: 4,
     name: 'Medieval Scene 4',
-    imageUrl: '/images/1664125463_shutok.ru.17210204.jpg'
+    imageUrl: '/medieval-easter-cards-new/images/1664125463_shutok.ru.17210204.jpg'
   },
   {
     id: 5,
     name: 'Medieval Scene 5',
-    imageUrl: '/images/1664125540_shutok.ru.17210187.jpg'
+    imageUrl: '/medieval-easter-cards-new/images/1664125540_shutok.ru.17210187.jpg'
   },
   {
     id: 6,
     name: 'Medieval Scene 6',
-    imageUrl: '/images/1664125541_shutok.ru.17210181.jpg'
+    imageUrl: '/medieval-easter-cards-new/images/1664125541_shutok.ru.17210181.jpg'
   },
   {
     id: 7,
     name: 'Medieval Scene 7',
-    imageUrl: '/images/1664125538_shutok.ru.17210173.jpg'
+    imageUrl: '/medieval-easter-cards-new/images/1664125538_shutok.ru.17210173.jpg'
   },
   {
     id: 8,
     name: 'Medieval Scene 8',
-    imageUrl: '/images/1664125480_shutok.ru.17210168.jpg'
+    imageUrl: '/medieval-easter-cards-new/images/1664125480_shutok.ru.17210168.jpg'
   },
   {
     id: 9,
     name: 'Medieval Scene 9',
-    imageUrl: '/images/1664125534_shutok.ru.17210163.jpg'
+    imageUrl: '/medieval-easter-cards-new/images/1664125534_shutok.ru.17210163.jpg'
   }
 ];
 
